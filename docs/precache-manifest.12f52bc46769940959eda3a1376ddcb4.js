661a61dae360dbe6e53b0f16b9a20804ea550897
Angel Ruiz <angelx430@gmail.com>
2020-08-24 08:05:04 -0400

    update index

---
+++ docs/precache-manifest.12f52bc46769940959eda3a1376ddcb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb51d1887ebc95b55de936ec7a20040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bffc63a371ce98d079f6",
+    "url": "/static/css/main.3eb48e8c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffc63a371ce98d079f6",
+    "url": "/static/js/main.ee5b5d88.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
